d63a6d8543101f40ff459a0bf55d68babd4d5f3b
Pedro Penha <pedro.penha.martins@gmail.com>
2021-04-10 16:30:52 -0300

    oasis pena incluida no mapa 1

--- Assets/Scripts/BoostPickUp.cs
+++ Assets/Scripts/BoostPickUp.cs
@@ -5,30 +5,20 @@ using UnityEngine;
 public class BoostPickUp : MonoBehaviour
 {
     [SerializeField] private GameObject trigger;
-    [SerializeField] private GameObject soundBoost;
-    private Coroutine boost;
+    [SerializeField] private Transform player;
 
-    // [SerializeField] private Transform player;
-
-    // void Update()
-    // {
-    //     var newRotation = Quaternion.LookRotation(player.transform.position - transform.position).eulerAngles;
-    //     newRotation.x = 0;
-    //     transform.rotation = Quaternion.Euler(newRotation);
-    // }
+    void Update()
+    {
+        var newRotation = Quaternion.LookRotation(player.transform.position - transform.position).eulerAngles;
+        newRotation.x = 0;
+        transform.rotation = Quaternion.Euler(newRotation);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            soundBoost.GetComponent<AudioSource>().Play();
-            boost = StartCoroutine(BoostPick());
             CircleStamina.instance.OasisStamina();
+            Destroy(trigger);
         }
-    }
-    private IEnumerator BoostPick(){
-        
-        flight.instance.Boost(10.0f);
-        yield return new WaitForSeconds(1);
-        Destroy(trigger);
     }
 }
